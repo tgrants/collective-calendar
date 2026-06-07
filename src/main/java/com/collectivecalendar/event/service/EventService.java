6787efd03113878ad264de207a89ac78265bde47
simonak-24 <simonchiks@gmail.com>
2026-06-07 20:08:23 +0300

    saiet?

--- src/main/java/com/collectivecalendar/event/service/EventService.java
+++ src/main/java/com/collectivecalendar/event/service/EventService.java
@@ -1,5 +1,8 @@
 package com.collectivecalendar.event.service;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 public interface EventService {
 	/**
 	 * Gets the time of next instance of an event at the given point in time.
--- src/main/java/com/collectivecalendar/event/service/EventServiceImpl.java
+++ src/main/java/com/collectivecalendar/event/service/EventServiceImpl.java
@@ -1,25 +1,29 @@
 package com.collectivecalendar.event.service;
 
 import com.collectivecalendar.model.Event;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class EventServiceImpl implements EventService  {
 
-	private final Event event;
+	private final Event event = null;
 	
 	@Override
-	ZonedDateTime getNextInstance(ZonedDateTime current_time) {
+	public ZonedDateTime getNextInstance(ZonedDateTime current_time) {
 		String frequencyString = event.getFrequency();
 		//
 		
-		return ZonedDateTime.now();;
+		return ZonedDateTime.now();
 		
 	}
 
 	@Override
-	List<ZonedDateTime> getAllInstances(ZonedDateTime current_time) {
-		return ZonedDateTime.now();
+	public List<ZonedDateTime> getAllInstances(ZonedDateTime current_time) {
+		return null;
 	}
 }
